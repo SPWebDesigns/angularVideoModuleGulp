3c058792f0b12f556d07db232308c651636ee192
Santiago Pitluk <spitluk@belatrix.com>
2016-10-07 03:57:37 -0300

    Final updates

--- src/client/app/dashboard/dashboard.controller.js
+++ src/client/app/dashboard/dashboard.controller.js
@@ -9,36 +9,39 @@
   /* @ngInject */
   function DashboardController($q, logger, videoList,  $sce) {
     var vm = this;
-    vm.players=[];
+    //videojs.players.dispose();
     vm.trustSrc = trustSrc;
-//    vm.player2 = videojs('example-video2');
-
-    
-
-    videoList.getvideoList().then(function(data) {
-        vm.data = data;
-        renderPlayers(vm.data);
-    })
-
+	
+	videoList.getvideoList().then(function(data) {
+	    vm.data = data;	
+	    setTimeout(function(){ 
+			renderBadPlayers(vm.data); 
+		}, 500);
+		setTimeout(function(){ 
+			renderGoodPlayers(vm.data); 
+		}, 500);   
+	})
+	
     function trustSrc(src) {
 	    return $sce.trustAsResourceUrl(src);
 	 }
+	 
+	 var renderBadPlayers= function(data){
+		for(var i=0; i<= data.bads.length;i++){
+  			videojs(data.bads[i].id).ready(function(){
+		        var player = this;
+			});
+	    }
+	}
 
-	 var renderPlayers= function(data){
-	 	angular.element(document).ready(function () {
-	         var i;
-	         for(i=0; i<=data.bads.length;i++){
-	         	var currentPlayer= data.bads[i].id;
-	         	console.log(videojs);
-	         	vm.players.push(videojs(currentPlayer));
-	         }
-	         console.log(vm.players);
-	   });
-	 	 // angular.element(document).ready(function () {
-	   //      vm.player3 = videojs('video3');
-	   //      vm.player4 = videojs('video4');
-	   //      vm.player5 = videojs('video5');
-	   //  });
-	 }
+	var renderGoodPlayers= function(data){
+		console.log('renderPlayers');
+		for(var j=0; j<= data.goods.length;j++){
+  			videojs(data.goods[j].id).ready(function(){
+		        var player = this;
+			});
+	    }
+	}
+
   }
 })();
